0c87b3c0f7af5ba115464d1636fefd17017fc581
doesDWQ <2354513578@qq.com>
2019-05-28 21:45:25 +0800

    Initial commit

---
+++ main.go
@@ -0,0 +1,100 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"github.com/chai2010/winsvc"
+	"log"
+	"net/http"
+	"os"
+	"path/filepath"
+	"time"
+)
+
+var (
+	server *http.Server
+)
+
+var (
+	appPath string
+	flagServiceName = flag.String("service-name", "myserver", "Set service name")
+	flagServiceDesc = flag.String("service-desc", "myserver service", "Set service description")
+	flagServiceInstall   = flag.Bool("service-install", false, "Install service")
+	flagServiceUninstall = flag.Bool("service-remove", false, "Remove service")
+	flagServiceStart     = flag.Bool("service-start", false, "Start service")
+	flagServiceStop      = flag.Bool("service-stop", false, "Stop service")
+)
+func init() {
+	// change to current dir
+	var err error
+	if appPath, err = winsvc.GetAppPath(); err != nil {
+		log.Fatal(err)
+	}
+	if err := os.Chdir(filepath.Dir(appPath)); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func main() {
+	flag.Parse()
+	// install service	安装服务
+	if *flagServiceInstall {
+		if err := winsvc.InstallService(appPath, *flagServiceName, *flagServiceDesc); err != nil {
+			log.Fatalf("installService(%s, %s): %v\n", *flagServiceName, *flagServiceDesc, err)
+		}
+		fmt.Printf("Done\n")
+		return
+	}
+	// remove service	删除服务
+	if *flagServiceUninstall {
+		if err := winsvc.RemoveService(*flagServiceName); err != nil {
+			log.Fatalln("removeService:", err)
+		}
+		fmt.Printf("Done\n")
+		return
+	}
+	// start service	开始服务
+	if *flagServiceStart {
+		if err := winsvc.StartService(*flagServiceName); err != nil {
+			log.Fatalln("startService:", err)
+		}
+		fmt.Printf("Done\n")
+		return
+	}
+	// stop service	停止服务
+	if *flagServiceStop {
+		if err := winsvc.StopService(*flagServiceName); err != nil {
+			log.Fatalln("stopService:", err)
+		}
+		fmt.Printf("Done\n")
+		return
+	}
+	// run as service	运行服务
+	if !winsvc.InServiceMode() {
+		log.Println("main:", "runService")
+		if err := winsvc.RunAsService(*flagServiceName, StartServer, StopServer, false); err != nil {
+			log.Fatalf("svc.Run: %v\n", err)
+		}
+		return
+	}
+	// run as normal
+	StartServer()
+}
+
+func StartServer() {
+	log.Println("StartServer, port = 8080")
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, "winsrv server", time.Now())
+	})
+	server = &http.Server{Addr: ":8080"}
+	server.ListenAndServe()
+}
+func StopServer() {
+	if server != nil {
+		server.Shutdown(context.Background()) // Go 1.8+
+	}
+	log.Println("StopServer")
+}
+
+
